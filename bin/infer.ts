6925b215048d9ad4f3141cf6b642e04ce9d60fb6
Andy Armstrong <andy@hexten.net>
2023-09-04 17:30:13 +0100

    Whoop! JSONPath.

--- bin/infer.ts
+++ bin/infer.ts
@@ -1,39 +1,45 @@
-type Index<K, T> = K extends `.${infer Key}`
+// Lookup types by path
+
+type ResolveKey<K, T> = K extends `.${infer Key}`
   ? Key extends keyof T ? T[Key] : never
   : K extends `[${infer Idx extends number}]`
     ? Idx extends keyof T ? T[Idx] : never
   : never;
 
-type IndexTerm = `.${string}` | `[${number}]`;
+type KeyTerm = `.${string}` | `[${number}]`;
 
-type IndexDeep<K, T> = K extends `${infer Head extends IndexTerm}[${infer Rest}`
-  ? IndexDeep<`[${Rest}`, Index<Head, T>>
-  : K extends `${infer Head extends IndexTerm}.${infer Rest}`
-    ? IndexDeep<`.${Rest}`, Index<Head, T>>
-  : Index<K, T>;
+type ResolveDeep<K, T> = K extends `${infer Head extends KeyTerm}[${infer Rest}`
+  ? ResolveDeep<`[${Rest}`, ResolveKey<Head, T>>
+  : K extends `${infer Head extends KeyTerm}.${infer Rest}`
+    ? ResolveDeep<`.${Rest}`, ResolveKey<Head, T>>
+  : ResolveKey<K, T>;
+
+type ResolvePath<JP, T> = JP extends `\$${infer Key}` ? ResolveDeep<Key, T>
+  : never;
+
+// Extract paths from type
 
 type FindNext<P extends string, T> = T extends
-  readonly unknown[] | Record<string, unknown> ? `${P}${Finder<T>}`
-  : `${P}`;
+  readonly unknown[] | Record<string, unknown> ? `${P}${Finder<T>}` : `${P}`;
 
 type ListPaths<T> = T extends readonly unknown[] ? {
     [K in keyof T]: K extends `${infer Idx extends number}`
-      ? FindNext<`[${Idx}]`, T[Idx]> /*  `[${Idx}]${Finder<T[Idx]>}`*/
+      ? FindNext<`[${Idx}]`, T[Idx]>
       : never;
   }[number]
   : never;
 
 type ObjectPaths<T> = T extends Record<string, unknown> ? {
-    [K in keyof T]: K extends string
-      ? FindNext<`.${K}`, T[K]> /*`.${K}${Finder<T[K]>}`*/
-      : never;
+    [K in keyof T]: K extends string ? FindNext<`.${K}`, T[K]> : never;
   }[keyof T]
   : never;
 
 type Finder<T> = ObjectPaths<T> | ListPaths<T>;
 
+type FindPaths<T> = `\$${Finder<T>}`;
+
 type Flatten<T> = {
-  [K in Finder<T>]: IndexDeep<K, T>;
+  [K in FindPaths<T>]: ResolvePath<K, T>;
 };
 
 const list = ["A", true, 3, {
@@ -43,6 +49,6 @@ const list = ["A", true, 3, {
 
 const obj = { list } as const;
 
-export type T0 = IndexDeep<".list[3].name", typeof obj>;
-export type T5 = Finder<typeof obj>;
+export type T0 = ResolvePath<"$.list[3].name", typeof obj>;
+export type T5 = FindPaths<typeof obj>;
 export type T6 = Flatten<typeof obj>;
