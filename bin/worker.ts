a818f952d5a3167baf3a8c1f6ae486eac97ffaf9
Andy Armstrong <andy@hexten.net>
2023-10-24 11:08:30 +0100

    Mo delete.

--- bin/worker.ts
+++
@@ -1,9 +0,0 @@
-/// <reference no-default-lib="true" />
-/// <reference lib="deno.worker" />
-
-import { Message } from "./threads.ts";
-
-self.onmessage = (ev: MessageEvent<Message>) => {
-  console.log(`worker:`, ev.data);
-  self.postMessage(ev.data);
-};
