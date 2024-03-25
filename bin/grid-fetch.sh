898460eceabc14e201def294059428a8aa69022f
Andy Armstrong <andy@hexten.net>
2024-03-25 13:19:49 +0000

    Add NEEB.

--- bin/grid-fetch.sh
+++ bin/grid-fetch.sh
@@ -3,10 +3,14 @@
 out="reports"
 
 while ssleep 60; do
+  # Powergrid
   name="$out/$(date '+%Y%m%d-%H%M%S').json"
   mkdir -p "$(dirname "$name")"
   echo "$name"
   deno run --allow-net bin/grid.ts > "$name"
+  # NEEB
+  name="$out/$(date '+%Y%m%d-%H%M%S').neeb.json"
+  curl -o "$name" "https://power.northernpowergrid.com/Powercut_API/rest/powercuts/getall"
 done
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
